605137fdb708fde3be3b88e47b21e16b49fc6fe7
Hasan Rahman <hasanrahman33605@gmail.com>
2025-02-17 22:20:49 +0600

    ...

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,12 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/logo.png";
+import { SignedIn, SignedOut, useUser, SignOutButton } from "@clerk/nextjs";
 
 function NavBar() {
+  const { isLoaded, isSignedIn, user } = useUser();
+  if (!isLoaded) return <p>Loading ...</p>;
   return (
     <>
       <nav className="fixed top-0 left-0 w-full bg-white shadow-sm z-50">
@@ -21,39 +25,58 @@ function NavBar() {
           {/* Navigation Links */}
           <div className="space-x-6 flex items-center">
             {/* Authentication Buttons */}
+            <SignedIn>
+              <Link
+                href="/mealplan"
+                className="text-gray-700 hover:text-emerald-500 transition-colors"
+              >
+                Mealplan
+              </Link>
+              {/* Profile Picture */}
+              {user?.imageUrl ? (
+                <Link href="/profile">
+                  <Image
+                    src={user.imageUrl}
+                    alt="Profile Picture"
+                    width={40}
+                    height={40}
+                    className="rounded-full"
+                  />
+                </Link>
+              ) : (
+                // Placeholder for users without a profile picture
+                <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
+              )}
 
-            <Link
-              href="/mealplan"
-              className="text-gray-700 hover:text-emerald-500 transition-colors"
-            >
-              Mealplan
-            </Link>
+              {/* Sign Out Button */}
+              <SignOutButton>
+                <button className="ml-4 px-4 py-2 bg-emerald-500 text-white rounded hover:bg-emerald-600 transition">
+                  Sign Out
+                </button>
+              </SignOutButton>
+            </SignedIn>
 
-            {/* Sign Out Button */}
+            <SignedOut>
+              <Link
+                href="/"
+                className="text-gray-700 hover:text-emerald-500 transition-colors"
+              >
+                Home
+              </Link>
+              <Link
+                href={isSignedIn ? "/subscribe" : "/sign-up"}
+                className="text-gray-700 hover:text-emerald-500 transition-colors"
+              >
+                Subscribe
+              </Link>
 
-            <button className="ml-4 px-4 py-2 bg-emerald-500 text-white rounded hover:bg-emerald-600 transition">
-              Sign Out
-            </button>
-
-            <Link
-              href="/"
-              className="text-gray-700 hover:text-emerald-500 transition-colors"
-            >
-              Home
-            </Link>
-            {/* <Link
-              href={isSignedIn ? "/subscribe" : "/sign-up"}
-              className="text-gray-700 hover:text-emerald-500 transition-colors"
-            >
-              Subscribe
-            </Link> */}
-
-            <Link
-              href="/sign-up"
-              className="px-4 py-2 bg-emerald-500 text-white rounded hover:bg-emerald-600 transition"
-            >
-              Sign Up
-            </Link>
+              <Link
+                href="/sign-up"
+                className="px-4 py-2 bg-emerald-500 text-white rounded hover:bg-emerald-600 transition"
+              >
+                Sign Up
+              </Link>
+            </SignedOut>
           </div>
         </div>
       </nav>
